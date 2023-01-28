c874136812999a66c45ae60a31e96409e62af319
Hristijan <hristijan93@hotmail.com>
2023-01-28 01:27:16 +0100

    simple crud app

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log('listening on 3000');    
+})
+
+// app.get('/', function(req, res) {
+
+// })
+
+console.log('May Node be with you');
